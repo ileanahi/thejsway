0cacab41c5ed85b5baf094508213c4a611059ce0
Ileana Argyris <argyris.ileana@gmail.com>
2019-02-02 21:46:09 +0200

    feat: Wrote a program that prompts users for their first and last name, then shows a greeting using their name

---
+++ chapter2.js
@@ -0,0 +1,4 @@
+// Write a program that asks the user for his first name and his last name
+let firstName = prompt("What is your first name?");
+let lastName = prompt("What is your last name?")
+alert(`Welcome, ${firstName} ${lastName}!`);
