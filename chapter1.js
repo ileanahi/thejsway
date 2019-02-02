a98b40e393d424530e93c8135f90d3a4f84273f0
Ileana Argyris <argyris.ileana@gmail.com>
2019-02-02 21:31:15 +0200

    feat: Wrote a program that displays the results of adding, subtracting, multiplying, and dividing 6 by 3

--- chapter1.js
+++ chapter1.js
@@ -1,3 +1,9 @@
 // Write a program that displays your name and age
 console.log("Ileana");
 console.log(24);
+
+// Write a program that displays the results of adding, subtracting, multiplying and dividing 6 by 3
+console.log(6+3);
+console.log(6-3);
+console.log(6*3);
+console.log(6/3);
