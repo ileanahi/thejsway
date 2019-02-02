b49ce4bcb0d2d22580badbb5c786142414e1c97a
Ileana Argyris <argyris.ileana@gmail.com>
2019-02-02 21:49:45 +0200

    feat: Added my predictions for the final values of the variables

--- chapter2.js
+++ chapter2.js
@@ -2,3 +2,17 @@
 let firstName = prompt("What is your first name?");
 let lastName = prompt("What is your last name?")
 alert(`Welcome, ${firstName} ${lastName}!`);
+
+// Predict the final values of the variables
+let a = 2;
+a -= 1;
+a++;
+let b = 8;
+b += 2;
+const c = a + b * b;
+const d = a * b + b;
+const e = a * (b + b);
+const f = a * b / a;
+const g = b / a * a;
+console.log(a, b, c, d, e, f, g);
+// a = 2, b = 10, c = 102, d = 30, e = 40, f = 10, g = 10
