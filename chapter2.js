a8f991439808dcaaad2015c9fe344adfb235d65a
Ileana Argyris <argyris.ileana@gmail.com>
2019-02-02 21:56:33 +0200

    feat: Wrote a program that prompts users for the raw price of an object and returns the total with 20.6% tax

--- chapter2.js
+++ chapter2.js
@@ -16,3 +16,8 @@ const f = a * b / a;
 const g = b / a * a;
 console.log(a, b, c, d, e, f, g);
 // a = 2, b = 10, c = 102, d = 30, e = 40, f = 10, g = 10
+
+// Write a program that asks the user for a raw price. After that, it calculates the corresponding final price using a VAT rate of 20.6%
+let price = prompt("How much does this cost?");
+let finalPrice = (price * 1.206).toLocaleString("en");
+alert(`Your total is $${finalPrice}`);
